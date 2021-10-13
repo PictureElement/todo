a7662bd2f1d7b04134d86cddc49213bcf87bf1b0
PictureElement <marios.sofokleous@yandex.com>
2021-10-13 12:09:43 +0300

    Apply the correct theme on new browser session.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-let currentTheme = localStorage.getItem('data-theme');
-
 const changeThemetoLight = () => {
     document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById('themeToggle').ariaLabel = 'Switch to the dark theme';
     localStorage.setItem("data-theme", "light");
 }
 
 const changeThemetoDark = () => {
     document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById('themeToggle').ariaLabel = 'Switch to the light theme';
     localStorage.setItem("data-theme", "dark");
 }
-
-const themeToggle = document.getElementById('themeToggle');
 
 themeToggle.addEventListener('click', function() {
     let theme = localStorage.getItem('data-theme');
 
     if (theme === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        this.ariaLabel = 'Switch to the light theme';
-        localStorage.setItem("data-theme", "dark");
+        changeThemetoDark();
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        this.ariaLabel = 'Switch to the dark theme';
-        localStorage.setItem("data-theme", "light");
+        changeThemetoLight();
     }
 });
+
+let currentTheme = localStorage.getItem('data-theme');
+
+if (currentTheme === 'light') {
+    changeThemetoLight();
+} else {
+    changeThemetoDark();
+}
